03de2781efca1709f33701f9388bbb9bf7262bc1
Srikarchaganti-01 <srikarchaganti134@gmail.com>
2026-04-01 19:37:37 +0530

    Create 1730-SpecialArrayWithXElementsGreaterThanOrEqualX

---
+++ 1730-SpecialArrayWithXElementsGreaterThanOrEqualX/1730-SpecialArrayWithXElementsGreaterThanOrEqualX.cpp
@@ -0,0 +1,21 @@
+// Last updated: 4/1/2026, 7:37:35 PM
+class Solution {
+public:
+    int specialArray(vector<int>& nums) {
+        int n = nums.size();
+        int low = 1;
+        int high = n;
+        while(low<=high){
+            int mid = (low+high)/2;
+            int count = 0;
+            for(int j =0;j<n;j++){
+                if(nums[j] >= mid) count++;
+            }
+            if(count == mid) return mid;
+            else if(count > mid) low = mid+1;
+            else if(count < mid) high = mid-1;
+            count = 0;
+        }
+        return -1;
+    }
+};
