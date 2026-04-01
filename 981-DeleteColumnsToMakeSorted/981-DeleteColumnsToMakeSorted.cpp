13ebe804cfb9c44e531aa40f57dc496061b25f7d
Srikarchaganti-01 <srikarchaganti134@gmail.com>
2026-04-01 19:37:59 +0530

    Create 981-DeleteColumnsToMakeSorted

---
+++ 981-DeleteColumnsToMakeSorted/981-DeleteColumnsToMakeSorted.cpp
@@ -0,0 +1,18 @@
+// Last updated: 4/1/2026, 7:37:57 PM
+class Solution {
+public:
+    int minDeletionSize(vector<string>& strs) {
+        int count = 0;
+        int r = strs.size();
+        int c = strs[0].size();
+        for(int i=0;i<c;i++){
+            for(int j=0;j<r-1;j++){
+                if(strs[j][i] > strs[j+1][i]){
+                    count++;
+                    break;
+                }
+            }
+        }
+        return count;
+    }
+};
