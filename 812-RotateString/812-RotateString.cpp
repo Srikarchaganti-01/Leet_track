1b74f869a858297f260af33756693ac6853f6fba
Srikarchaganti-01 <srikarchaganti134@gmail.com>
2026-04-01 19:38:03 +0530

    Create 812-RotateString

---
+++ 812-RotateString/812-RotateString.cpp
@@ -0,0 +1,19 @@
+// Last updated: 4/1/2026, 7:38:01 PM
+class Solution {
+public:
+    bool rotateString(string s, string goal) {
+        if(s.size() != goal.size()) return false;
+        for(int i =0;i<s.size();i++){
+            bool match = true;
+            for(int j=0;j<s.size();j++){
+                int k = (j+i) % s.size();
+                if(s[k] != goal[j]) {
+                    match = false;
+                    break;
+                }
+            }
+            if (match) return true;
+        }
+        return false;
+    }
+};
