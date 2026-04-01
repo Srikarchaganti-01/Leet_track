c24ccc14d50cd7a6dce3491f3d4a17309eb94f94
Srikarchaganti-01 <srikarchaganti134@gmail.com>
2026-04-01 19:37:23 +0530

    Create 2219-MaximumNumberOfWordsFoundInSentences

---
+++ 2219-MaximumNumberOfWordsFoundInSentences/2219-MaximumNumberOfWordsFoundInSentences.cpp
@@ -0,0 +1,15 @@
+// Last updated: 4/1/2026, 7:37:21 PM
+class Solution {
+public:
+    int mostWordsFound(vector<string>& sentences) {
+        int maxi = INT_MIN;
+        for (int i = 0; i < sentences.size(); i++) {
+            int count = 1; 
+            for (int j = 0; j < sentences[i].size(); j++) {
+                if (sentences[i][j] == ' ') count++;
+            }
+            maxi = max(maxi, count);
+        }
+        return maxi;
+    }
+};
