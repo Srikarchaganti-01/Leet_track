fc7958a359a69c1fc22f5b8317233cdc9790db47
Srikar Chaganti <srikarchaganti134@gmail.com>
2026-04-16 23:34:24 +0530

    Create 528-SwappingNodesInALinkedList

---
+++ 528-SwappingNodesInALinkedList/528-SwappingNodesInALinkedList.cpp
@@ -0,0 +1,30 @@
+// Last updated: 4/16/2026, 11:34:23 PM
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* swapNodes(ListNode* head, int k) {
+            ListNode* temp = head;
+            int n = 0;
+            while(temp != nullptr){
+                n++;
+                temp = temp->next;
+            }
+            int leftPos = k;
+            int rightPos = n - k + 1;
+            ListNode* leftNode = head;
+            ListNode* rightNode = head;
+            for(int i = 1; i < leftPos; i++) leftNode = leftNode->next;
+            for(int i = 1; i < rightPos; i++) rightNode = rightNode->next;
+            swap(leftNode->val, rightNode->val);
+            return head;
+    }
+};
