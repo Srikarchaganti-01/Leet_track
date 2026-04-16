6b72518f95d6fd8351c96dbcfdb88af6c0442775
Srikar Chaganti <srikarchaganti134@gmail.com>
2026-04-16 23:33:22 +0530

    Create 4280-CountDigitAppearances

---
+++ 4280-CountDigitAppearances/4280-CountDigitAppearances.cpp
@@ -0,0 +1,15 @@
+// Last updated: 4/16/2026, 11:33:22 PM
+class Solution {
+public:
+    int countDigitOccurrences(vector<int>& nums, int digit) {
+        int count = 0;
+        for(int i = 0;i<nums.size();i++){
+            int num = nums[i];
+            while(num>0){
+                if(num%10 == digit) count++;
+                num = num/10;
+            }
+        }
+        return count;
+    }
+};
