5b3920a0cbc9d4fde97645045d14f836604a1a99
Srikar Chaganti <srikarchaganti134@gmail.com>
2026-04-16 23:33:51 +0530

    Create 2299-MergeNodesInBetweenZeros

---
+++ 2299-MergeNodesInBetweenZeros/2299-MergeNodesInBetweenZeros.cpp
@@ -0,0 +1,43 @@
+// Last updated: 4/16/2026, 11:33:50 PM
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergeNodes(ListNode* head) {
+        ListNode* temp = head;
+        queue<int>v;
+        int counter =0;
+        int count  = 0;
+        while(temp!=nullptr){
+            if(temp->val == 0){
+                if(counter == 0){
+                    counter = 1;
+                    count = 0;
+                }
+                else if(counter == 1){
+                    v.push(count);
+                    counter = 0;
+                    continue;
+                }
+            }
+            count = count+temp->val;
+            temp = temp->next;
+        }
+        ListNode* newnode = new ListNode(0);
+        temp = newnode;
+        while(!v.empty()){
+            temp->next = new ListNode(v.front());
+            temp = temp->next;
+            v.pop();
+        }
+        return newnode->next;
+    }
+};
